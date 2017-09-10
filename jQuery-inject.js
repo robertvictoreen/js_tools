61f05a9b824653e50d4f806857ed53c5b8934dac
robertvictoreen <31823681+robertvictoreen@users.noreply.github.com>
2017-09-10 23:11:22 +0800

    Add files via upload

---
+++ jQuery-inject.js
@@ -0,0 +1,3 @@
+(function(s,o,g,a,m){a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(document,'script','https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js');
---
+++ stopAllTimers.js
@@ -0,0 +1,4 @@
+var maxId = setTimeout(function(){}, 0);
+for(var i=0; i < maxId; i+=1) { 
+    clearTimeout(i);
+}
